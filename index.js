09feb217002e6bab781dee371dfc28e503208b85
unknown <shann1990@hotmail.co.uk>
2015-04-29 23:54:25 +0100

    View and Dashbord added, socket IO script added

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
 var express = require('express');
 var app = express();
 
-app.get('/',function(req,res){
-	res.send("Hello World")
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+app.use(express.static('public'));
+
+server.listen(3000, function(){
+	console.log("App is listening on port 3000");
 });
 
-var server = app.listen(3000, function(){
+var users = {};
+var userCount= 0;
 
-	var host = server.address().address;
-	var port = server.address().port;
+var poker = io.of('/poker');
+var dashboard = io.of('/dashboard');
 
-	console.log("App is listening on port 3000");
-})
+
+poker.on('connection',function(socket){
+	console.log(socket.id + ' : (Poker) A user has connection Hurraayyyy!!!');
+	userCount ++;
+	socket.on('disconnect',function(){		
+		console.log(socket.id + ' : (Poker) User has left');
+		delete users[socket.id];
+		userCount--;
+		dashboard.emit('user-count',userCount);
+		dashboard.emit('users',users);
+	})
+	socket.on('user-data',function(data){		
+		users[socket.id] = data.name;
+		socket.emit('user-count',userCount);
+		dashboard.emit('user-count',userCount);
+		dashboard.emit('users',users);
+	});
+});
+
+dashboard.on('connection',function(socket){
+	console.log(socket.id + ' : (Dashboard) A user has connection Hurraayyyy!!!');
+	socket.on('disconnect',function(){		
+		console.log(socket.id + ' : (Dashboard) User has left');
+	})
+	console.log(userCount);
+	socket.emit('user-count', userCount);
+	socket.emit('users', users);
+});
